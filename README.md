5b006b43fcf252571fc0a5ef9353aca95f649057
Ethan Cox <ethanrcox@gmail.com>
2023-03-04 11:20:54 -0500

    pushing before working from other laptop

--- README.md
+++ README.md
@@ -2,11 +2,15 @@
 
 
 # Options
-1. Split by delimiter(s) (default=all whitespace)
+1. Split by delimiter(s) characters provided as a string (default=all whitespace)
  - Remove empty entries (default=true)
  - Trim whitespace (default=true)
  - Case insensitive? (default=false)
 2. Split on size (index)
 3. Split into MAX/MIN strings
 4. Split on [regular expression](https://crates.io/crates/regex)
-2. Output field separator (string, default = "\n" aka `0x0a`)
+5. Output field separator (string, default = "\n" aka `0x0a`)
+6. Process lines individually. 
+
+# Future Plans
+- Split on strings, not just chars (employ [bpsm](https://www.cs.haifa.ac.il/%7Eoren/Publications/bpsm.pdf))
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use clap::{Command, Arg, ArgAction};
+use std::collections::HashSet;
 
 fn main() -> io::Result<()> {
     let args = Command::new("txsplit")
@@ -7,11 +8,28 @@ fn main() -> io::Result<()> {
                     .version("1.0")
                     .about("Splits text on a delimiter")
                     .arg(
-                        Arg::new("delimiter")
+                        Arg::new("input-delimiter")
+                            .long("input-delimiter")
+                            .short('d')
+                            .help("the delimiter(s) to split the input on")
+                    )
+                    .arg(
+                        Arg::new("output-delimiter")
+                        .long("output-delimiter")
+                        .short('D')
+                        .help("the delimiter to print after each split entry")
                     ).get_matches();
+
+    let input_delimiters: HashSet<char> = HashSet::from_iter(args.get_one::<String>("input-delimiter").unwrap_or(&String::from(" \t\n\r")).chars());
+    let output_delimiter = args.get_one::<String>("output-delimiter").unwrap_or(&String::from("\n"));
 
     for line in io::stdin().lines() {
         let line = line?;
+        let mut lptr = 0;
+
+        for (rptr, ch) in line.chars().enumerate() {
+            lptr = rptr;            
+        }
     }
 
     Ok(())
